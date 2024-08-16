19cb66cf33ed7717c492218310d7cf0b1fcf24ff
SmitAnn <117157995+SmitAnn@users.noreply.github.com>
2024-08-16 16:50:22 +0530

    java file add

---
+++ OddEvenNumber.java
@@ -0,0 +1,17 @@
+public class OddEvenNumber
+{
+  public static void main(String [] args)
+   {
+   int n= 5 , m=20;
+   double res=(double)n+m;
+   System.out.println("Result =   "  + res);
+   if (res%2==0)
+    {
+	 System.out.println("Result number is Even.");
+	}
+	else
+	{
+	  System.out.println("Result number is ODD.");
+	}
+   }
+}
